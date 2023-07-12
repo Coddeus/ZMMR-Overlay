f144c44423897332b1f9981b548a8dbdcc286736
Coddeus <codeusbonus@gmail.com>
2023-07-12 23:56:09 +0200

    🍹 Scaled the tranparency to the whole app Added the number of locations explored "0 / 395" New font with custom italic New background color

--- main.gd
+++ main.gd
@@ -3,7 +3,7 @@ extends Control
 func _ready():
 	get_tree().get_root().transparent_bg = true
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("esc"):
 		get_tree().quit()
 	return
